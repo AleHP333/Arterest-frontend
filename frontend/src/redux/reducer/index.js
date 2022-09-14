9e607df8e89f7970ad32eb53e2dd4ee14714662d
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2022-09-14 09:39:07 -0300

    [config/redux/dependencies/app/index]

---
+++ frontend/src/redux/reducer/index.js
@@ -0,0 +1,22 @@
+const initialState = {
+
+    
+    products: [],
+    allProducts:[]
+   
+}
+
+export default function rootReducer(state = initialState, action) {
+    switch(action.type) {
+        
+        case 'GET_ALL_PRODUCTS':
+        return {
+            ...state,
+            products: action.payload,
+            allProducts:[ ...action.payload]
+        }
+        default: 
+        return state
+
+}
+}
