7e1773dccce401b412c2f8bcb087d908751c5677
David-G18 <kdavidg1998@gmail.com>
2022-09-16 10:18:01 -0500

    component detail

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './app.css'
 import {BrowserRouter as Router,  Routes, Route} from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage'
 import PostProduct from './pages/postProduct/PostProduct';
-
+import DetailProduct from './components/DetailProduct/DetailProduct';
 
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
       <Router>
         <Routes>
 
-          <Route exact path="/" element={<LandingPage/>}/>
+          <Route exact path="/" element={<LandingPage/>} />
           <Route exact path="/creation" element={<PostProduct />} />
-        
+          <Route exact path ="/detail/:id" element={<DetailProduct/>} />
         
 
         </Routes>
