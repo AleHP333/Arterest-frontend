3d51df1050f9ae44b9ddde50db00cfbfb640792d
Anibal Alvarez <alvarezanbl@gmail.com>
2022-09-16 12:15:43 -0300

    nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,11 +10,17 @@ export default function NavBar(){
                 <h1>Home</h1>
             </Link>
             <Searchbar />
+            <Link to='/favorites'>
+                <h1>fav</h1>
+            </Link>
             <Link to='/login'>
                 <h1>Log In</h1>
             </Link>
             <Link to='/signup'>
                 <h1>Sign Up</h1>
+            </Link>
+            <Link to='/cart'>
+                <h1>cart</h1>
             </Link>
         </nav>
     )
