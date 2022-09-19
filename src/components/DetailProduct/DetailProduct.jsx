efd2bfa2ceeaeba07bbacb7a348e9f9e2a7bdbbd
David-G18 <kdavidg1998@gmail.com>
2022-09-19 00:00:18 -0500

    Sprint 1 changes detail

--- src/components/DetailProduct/DetailProduct.jsx
+++ src/components/DetailProduct/DetailProduct.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+// Actions
 import { getPaintById } from '../../redux/actions/productActionsTest';
+// Styles
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import ShareIcon from '@mui/icons-material/Share';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import './DetailProduct.css'
 
 export default function DetailProduct () {
@@ -11,16 +20,32 @@ export default function DetailProduct () {
     const paint = useSelector((state) => state.testReducer.paintDetail);
 
     useEffect(() => {
-        dispatch(getPaintById(id));
-    }, [dispatch, id]);
+        if (!paint || id !== paint._id) {
+            dispatch(getPaintById(id));
+        }
+    }, [dispatch, id, paint]);
 
-    console.log(paint);
+    // Alert Logic 
+    const [open, setOpen] = React.useState(false);
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+    const handleClick = () => {
+        setOpen(true);
+    };
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+        setOpen(false);
+    };
+
 
     return (
-        !paint ? <div>loading...</div> :
-        <div className="bg-white">  
+        !paint || id !== paint._id ? <CircularProgress /> :
+        <div className="containerDetail bg-white">  
             <div className="p-8 text-gray-600 md:px-12 xl:px-8">
-                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                <div className="md:flex md:gap-6 lg:justify-center lg:gap-12">
                     {/* Esta parte es un image magnifier, se necesita mejorar su uso, asi que por ahora no esta habilitado 
                     <div className="md:5/12 lg:w-4/12" 
                     data-role="imagemagnifier"
@@ -29,13 +54,31 @@ export default function DetailProduct () {
                     data-lens-size="200">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Leonardo_da_Vinci_-_Mona_Lisa_%28Louvre%2C_Paris%29.jpg/800px-Leonardo_da_Vinci_-_Mona_Lisa_%28Louvre%2C_Paris%29.jpg" alt="productImage" loading="lazy" width="" height="100px" />
                     </div> */}
-                    <div className="md:5/12 lg:w-4/12">
-                        <img alt="" src={paint.userImage} loading="lazy" width="" height="" />
+                    <div className="md:w-6/12 lg:w-4/12">
+                        <img alt="" src={paint.img} loading="lazy" width="" height="" />
                     </div>
-                    <div className="m-auto md:7/12 lg:w-6/12">
-                    <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
-                    <p className="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-                    <p className="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+                    <div className="mt-2 lg:flex lg:flex-col md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">{paint.title}</h2>
+                        <p className="mt-6 text-gray-600">{paint.description}</p>
+                        <div className='flex mt-6 gap-3'>
+                            <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white" src={`${paint.userImage}`} alt=""></img>
+                            <p className="self-center text-gray-400">By {paint.userName}</p>
+                        </div>
+                        <div className='flex gap-6 mt-6 self-center'>
+                            <CopyToClipboard text={window.location.href}>
+                                <Button onClick={handleClick} variant="contained" startIcon={<ShareIcon />}>
+                                        Share
+                                </Button>
+                            </CopyToClipboard>
+                            <Button variant="contained" startIcon={<FavoriteIcon />}>
+                                    Favorite
+                            </Button>
+                            <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+                                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                                Link copied to clipboard 
+                                </Alert>
+                            </Snackbar>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/redux/reducers/testReducer.js
+++ src/redux/reducers/testReducer.js
@@ -9,48 +9,48 @@ const initialState = {
 }
 
 export default function testReducer(state = initialState, action) {
-  switch (action.type) {
-    case "GET_ALL_PRODUCTS":
-      return {
-        ...state,
-        products: action.payload,
-        allProducts: [...action.payload],
-        isLoading: false,
-      };
-    case "ART_FILTER":
-      return {
-        ...state,
-        allProducts: action.payload,
-        isLoading: false,
-      };
-    case "ACTIVE_LOADING":
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case "GET_PRODUCT_SEARCHBAR":
-      return {
-        ...state,
-        products: action.payload,
-        allProducts: action.payload,
-      };
-    case "GET_AN_ARTIST":
-      return {
-        ...state,
-        getAnArtist: action.payload,
-        isLoading: true,
-      };
-    case "REMOVE_ARTIST_SELECTED":
-      return {
-        ...state,
-        getAnArtist: [],
+    switch (action.type) {
+        case "GET_ALL_PRODUCTS":
+        return {
+            ...state,
+            products: action.payload,
+            allProducts: [...action.payload],
+            isLoading: false,
+        };
+        case "ART_FILTER":
+        return {
+            ...state,
+            allProducts: action.payload,
+            isLoading: false,
+        };
+        case "ACTIVE_LOADING":
+        return {
+            ...state,
+            isLoading: true,
+        };
+        case "GET_PRODUCT_SEARCHBAR":
+        return {
+            ...state,
+            products: action.payload,
+            allProducts: action.payload,
+        };
+        case "GET_AN_ARTIST":
+        return {
+            ...state,
+            getAnArtist: action.payload,
+            isLoading: true,
+        };
+        case "REMOVE_ARTIST_SELECTED":
+        return {
+            ...state,
+            getAnArtist: [],
+        };
         case GET_PAINT_BY_ID:
-            return {
-                ...state,
-                paintDetail: action.payload,
-            }
-      };
-    default:
-      return state;
-  }
+        return {
+            ...state,
+            paintDetail: action.payload,
+        }
+        default:
+            return state;
+    }
 }
