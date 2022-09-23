db6a146bf7699df621e743e5763b870e6c0a7cb5
Carlos7847 <carlitosmp1597g@gmail.com>
2022-09-23 12:40:49 -0500

    borrado lógico, ban

--- src/redux/actions/productActionsTest.js
+++ src/redux/actions/productActionsTest.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:3001"
-
+const url = "http://localhost:3001";
 
 export const GET_PAINT_BY_ID = "GET_PAINT_BY_ID";
 
@@ -20,18 +19,16 @@ export function getPaintById(id) {
     const res = await axios.get(`${url}/paints/getOnePaint/${id}`);
     dispatch({
       type: GET_PAINT_BY_ID,
-      payload: res.data
-    })
-  }
+      payload: res.data,
+    });
+  };
 }
 
 export const getProductSearchbar = (input) => (dispatch) => {
   console.log("hola entre a la accion");
   async function search(dispatch) {
     console.log("hola entre al dispatch");
-    const { data } = await axios.get(
-      `${url}/paints/allpaints?art=${input}`
-    );
+    const { data } = await axios.get(`${url}/paints/allpaints?art=${input}`);
     console.log(data);
     dispatch({
       type: "GET_PRODUCT_SEARCHBAR",
@@ -43,9 +40,7 @@ export const getProductSearchbar = (input) => (dispatch) => {
 
 export function artFilterByBack(payload) {
   return async function (dispatch) {
-    const response = await axios.get(
-      `${url}/searchFilters?${payload}`
-    );
+    const response = await axios.get(`${url}/searchFilters?${payload}`);
     dispatch({
       type: "ART_FILTER_BY_BACK",
       payload: response.data,
@@ -59,9 +54,7 @@ export function activeLoading() {
 
 export function getAnArtist(userName) {
   return async function (dispatch) {
-    const res = await axios.get(
-      `${url}/paints/allpaints?art=${userName}`
-    );
+    const res = await axios.get(`${url}/paints/allpaints?art=${userName}`);
     console.log(res.data, "SOY EL USERNAME");
     dispatch({
       type: "GET_AN_ARTIST",
@@ -70,46 +63,55 @@ export function getAnArtist(userName) {
   };
 }
 
-export function getComments(paintId){
-  return async function(dispatch){
-      const comments = await axios.get(`${url}/likeComments/getPaintComments/${paintId}`)
-      dispatch({type: "GET_COMMENTS", payload: comments.data.response.comments })
-      return comments.data.response.comments.reverse()
-  }
+export function getComments(paintId) {
+  return async function (dispatch) {
+    const comments = await axios.get(
+      `${url}/likeComments/getPaintComments/${paintId}`
+    );
+    dispatch({
+      type: "GET_COMMENTS",
+      payload: comments.data.response.comments,
+    });
+    return comments.data.response.comments.reverse();
+  };
 }
 
-export function artFilter(price){
-  return {type: "ART_FILTER", payload: price}
+export function artFilter(price) {
+  return { type: "ART_FILTER", payload: price };
 }
 
 export function getUserById(id) {
   return async function (dispatch) {
     const res = await axios.get(`http://localhost:3001/user/${id}`);
     dispatch({
-      type: 'GET_USER_BY_ID',
-      payload: res.data
-    })
-  }
+      type: "GET_USER_BY_ID",
+      payload: res.data,
+    });
+  };
 }
 
 export function getAllUsers(id) {
   return async function (dispatch) {
-    const res = await axios.get(`http://localhost:3001/user/`);
+    const res = await axios.get(
+      `http://localhost:3001/adminActions/getAllUsers`
+    );
     dispatch({
-      type: 'GET_ALL_USERS',
-      payload: res.data
-    })
-  }
+      type: "GET_ALL_USERS",
+      payload: res.data,
+    });
+  };
 }
 
 export const updateProduct = (artwork) => {
   return async function (dispatch) {
-      const response = await axios.put(`http://localhost:3001/paints/allpaints?art=${artwork._id}`,
-          artwork);
-          console.log(response)
-      return dispatch({
-          type: 'UPDATE_PRODUCT',
-          payload: response.data
-      });
+    const response = await axios.put(
+      `http://localhost:3001/paints/allpaints?art=${artwork._id}`,
+      artwork
+    );
+    console.log(response);
+    return dispatch({
+      type: "UPDATE_PRODUCT",
+      payload: response.data,
+    });
   };
 };
