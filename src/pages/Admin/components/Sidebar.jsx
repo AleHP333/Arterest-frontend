240f5e8589c653baf0b3b0e31538d7beaabd0ab3
Mariana Stocco <mariana.stocco@outlook.com>
2022-09-25 10:41:26 -0300

    more

--- src/pages/Admin/components/Sidebar.jsx
+++ src/pages/Admin/components/Sidebar.jsx
@@ -123,7 +123,7 @@ export default function Sidebar() {
                 <Link
                   className="text-blueGray-300 text-xs uppercase py-3 font-bold block"
                   to="/creation"
-                  onClick={e => e.preventDefault()}
+                  
                 >
                   <i className="fas fa-clipboard-list text-blueGray-300 mr-2 text-sm"></i> Create Artwork
                 </Link>
