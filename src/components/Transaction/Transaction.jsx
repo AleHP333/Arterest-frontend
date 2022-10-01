b901889fabd592356053d870b783f1b9a7e59d08
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2022-10-01 18:36:27 -0300

    souUmCraque

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,26 +1,22 @@
 import React, { useEffect } from "react";
 import swal from "sweetalert";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import  "./Tran.css";
+import "./Tran.css";
 import { Button } from "react-bootstrap";
-import axios from 'axios';
+import i from "../../assets/stack.png"
 
 
 export default function Transaction() {
   const navigate = useNavigate();
-  
+
   // const transact = async () => {
   //  let transaction = await axios(`http://localhost:3001/capture-order`);
   //   return transaction.data.data
-    
-   
-       
+
   // };
 
-  const transact = "success"
+  const transact = "success";
 
-  
   useEffect(() => {
     if (transact === "success") {
       swal({
@@ -33,27 +29,37 @@ export default function Transaction() {
           icon: "error",
         });
       }
-      
     }
   }, []);
 
   return (
     <div>
+
       {!transact ? (
         <h1>Loading</h1>
-      ) : (
-        <div className="container" >
-          <h1>Back to:</h1>
-          <div className="container_buttons" >
-            <br />
-            <br />
-            <Button className="butt" onClick={() => navigate("/home")}>Home</Button>
-            <Button className="butt" onClick={() => navigate("/profile/history")}>
-             Buy History
-            </Button>
+        ) : (
+          <div className="container" >
+          <div>
+            <h1 className="container_buttons">Back to:</h1>
+            <div className="container_buttons">
+              <br />
+              <br />
+              <Button className="butt" onClick={() => navigate("/home")}>
+                Home
+              </Button>
+              <Button
+                className="butt"
+                onClick={() => navigate("/profile/history")}
+              >
+                Buy History
+              </Button>
+            </div>
           </div>
         </div>
       )}
+          <img className="pepe2" src= {i}>
+
+          </img>
     </div>
   );
 }
