64f62b6c117311f152bb413489a33a3b00cd1141
AleHP333 <102203389+AleHP333@users.noreply.github.com>
2022-10-03 17:49:30 -0300

    URL UPDATED

--- src/redux/actions/userSignActions.js
+++ src/redux/actions/userSignActions.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const url = "http://localhost:3001"  
+const url = "https://arterest-back.herokuapp.com"  
 
 export function signIn(emailAndPass){
     return async function(dispatch){
