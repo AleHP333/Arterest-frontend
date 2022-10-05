180dc621d911903a7859998bd9f257772d5b5171
David-G18 <kdavidg1998@gmail.com>
2022-10-04 21:57:13 -0500

    Like animation and skeleton

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,5 @@
 // From React
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // Icons
 import { AiFillShopping } from "react-icons/ai";
@@ -27,20 +27,31 @@ export default function Card({
   setFavProducts,
 }) {
 
+  const [isImageLoaded, setImageIsLoaded] = useState(false);
+
   return (
     <div className="container rounded-lg mb-5" key={_id}>
       <div className="img-container">
-        {
-          !img ? 
-          <Skeleton variant="circular" width={40} height={40} /> : 
-          <Link to={`/detail/${_id}`}>
-            <img className="w-full" src={img} alt="hola" />
-          </Link>
-        }
+
+        <Link to={`/detail/${_id}`}>
+            { !isImageLoaded && 
+              <Skeleton 
+                animation="wave" 
+                variant="rounded" 
+                width="100%"
+                height={300} 
+              /> }
+            <img 
+              className={`w-full ${isImageLoaded ? "flex" : "hidden"}`} 
+              src={img} 
+              alt={title}
+              onLoad={() => setTimeout(() => setImageIsLoaded(true), 2000)}
+            />
+        </Link>
         
         <div className="user-info bg-white p-3 flex flex-col gap-4">
           <div className="flex justify-center items-center">
-            <h2 className="font-semibold text-xl">{title}</h2>
+            <h2 className="font-semibold text-xl w-52 ">{title}</h2>
             <div className="flex text-center justify-start gap-1 absolute left-4">
               <FavoriteIcon className='text-red-500'/>
               <span className="text-gray-600 relative bottom-0.5">{cardLikes}</span>
